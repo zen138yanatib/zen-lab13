44548131ba05717236aa163bbb17aaf8d9e57f0b
zen138yanatib <64641188+zen138yanatib@users.noreply.github.com>
2021-01-22 17:44:41 +0700

    Update lab13_1.cpp

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -3,7 +3,22 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+   int i, j; 
+    double key; 
+    for (i = 1; i < N; i++){  
+        key = d[i];  
+        j = i - 1;  
+    while (j >= 0 && d[j] < key){  
+            d[j + 1] = d[j];  
+            j--;  
+    }  
+        d[j + 1] = key;
+        cout << "Pass " << i << ":";
+        for(int i = 0; i < 10; i++){
+            cout << d[i] << " ";
+        }
+        cout << "\n";
+    }  
 }
 
 int main(){
